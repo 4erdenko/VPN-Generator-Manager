224eca16dd2a7449a6f9f60c60459545a256d140
Valentin <valentin-kharenko@tuta.io>
2023-07-30 21:20:30 +0300

    fixes

--- fastapi_app/db/database.py
+++ fastapi_app/db/database.py
@@ -1,8 +1,6 @@
-# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# database.py
 SQLALCHEMY_DATABASE_URL = (
     'postgresql://username:password@localhost:5432' '/database'
 )
--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,19 +1,19 @@
 import json
 import os
 from datetime import date, datetime
-from typing import Any, List, Optional
+from typing import Any, Dict, Generator, List, Optional, Union
 
-from db.database import SessionLocal
-from db.models import Log
 from fastapi import BackgroundTasks, Depends, FastAPI, HTTPException, Request
 from fastapi.responses import FileResponse, JSONResponse, StreamingResponse
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
+from db.database import SessionLocal
+from db.models import Log
 from vpnworks.api import VpnWorksApi
 
 
-def get_db():
+def get_db() -> Generator:
     db = SessionLocal()
     try:
         yield db
@@ -25,7 +25,7 @@ app = FastAPI()
 api = VpnWorksApi()
 
 
-def remove_file(filename: str):
+def remove_file(filename: str) -> None:
     os.remove(filename)
 
 
@@ -33,8 +33,9 @@ def user_not_found():
     raise HTTPException(status_code=404, detail='User not found')
 
 
+
 @app.middleware('http')
-async def log_request(request: Request, call_next):
+async def log_request(request: Request, call_next) -> Union[StreamingResponse, FileResponse, JSONResponse]:
     response = await call_next(request)
 
     db = SessionLocal()
@@ -48,7 +49,7 @@ async def log_request(request: Request, call_next):
         response_info['filename'] = response.filename
     elif isinstance(response, JSONResponse):
         response_info['response_type'] = 'json'
-        response_info['content'] = response.content.decode('utf-8')
+        response_info['content'] = response.body.decode('utf-8')
     else:
         response_info['response_type'] = 'regular'
         response_info['content'] = response.body.decode('utf-8')
@@ -68,29 +69,30 @@ async def log_request(request: Request, call_next):
 
 
 @app.get('/token', description='Retrieve the API token')
-async def get_token():
+async def get_token() -> JSONResponse:
     token = await api.token
     return JSONResponse({'Bearer token': token})
 
 
 @app.get('/users', description='Retrieve a list of all users')
-async def get_users():
+async def get_users() -> JSONResponse:
     users = await api.get_users()
     return JSONResponse(users)
 
 
 @app.get('/user/id/{id}', description='Retrieve a user by their ID')
-async def get_user_by_id(id: int):
+async def get_user_by_id(id: int) -> Dict[str, Any]:
     response = await get_users()
     users = json.loads(response.body)
     try:
         return users[id]
     except IndexError:
-        return user_not_found()
+        user_not_found()  # Just call the function to raise the exception
+
 
 
 @app.get('/user/name/{name}', description='Retrieve a user by their name')
-async def get_user_by_name(name: str):
+async def get_user_by_name(name: str) -> Union[Dict[str, Any]]:
     users_dict = await api.get_users_dict()
     user = users_dict.get(name)
     if not user:
@@ -99,18 +101,15 @@ async def get_user_by_name(name: str):
 
 
 @app.delete('/user/delete/{user_id}', description='Delete a user by their ID')
-async def delete_user(user_id: str):
+async def delete_user(user_id: str) -> Union[int]:
     result_code = await api.delete_user(user_id)
     if result_code == 403:
         return user_not_found()
     return result_code
 
 
-@app.get(
-    '/user/{name}/stats',
-    description='Get personal stats for user via username',
-)
-async def get_personal_stats(name: str):
+@app.get('/user/{name}/stats', description='Get personal stats for user via username')
+async def get_personal_stats(name: str) -> Union[Dict[str, Dict[str, Union[str, int, None]]]]:
     users = await api.get_users_dict()
     user = users.get(str(name))
     if user:
@@ -131,22 +130,19 @@ async def get_personal_stats(name: str):
 
 
 @app.get('/config', description='Create and download user conf file')
-async def get_config_file(background_tasks: BackgroundTasks):
+async def get_config_file(background_tasks: BackgroundTasks) -> FileResponse:
     filename, _ = await api.create_conf_file()
     background_tasks.add_task(remove_file, filename)
     return FileResponse(filename, filename=filename)
 
 
-
-
-
 class LogOut(BaseModel):
     request_date: datetime
-    request_path: str
-    request_method: str
-    request_args: dict
-    response_status: int
-    response_body: dict[Optional[str], Any]
+    request_path: Optional[str]
+    request_method: Optional[str]
+    request_args: Optional[dict]
+    response_status: Optional[int]
+    response_body: Optional[Dict[Optional[str], Any]]
 
     class Config:
         from_attributes = True
@@ -160,7 +156,7 @@ async def get_logs(
     offset: int = 0,
     order: Optional[str] = 'desc',
     db: Session = Depends(get_db),
-):
+) -> List[LogOut]:
     if order not in ['asc', 'desc']:
         raise HTTPException(
             status_code=400, detail="Order must be either 'asc' or 'desc'"
@@ -186,4 +182,6 @@ async def get_logs(
 
     logs = logs_query.limit(limit).offset(offset).all()
 
-    return logs
+    log_outs = [LogOut(**log.__dict__) for log in logs]
+
+    return log_outs
